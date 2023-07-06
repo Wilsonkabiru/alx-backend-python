00a12b8b1ebdf3461733107b6d8288ed8a0870af
Carlos Wilson Niqx <wkabiru19@gmail.com>
2023-07-06 21:12:40 +0300

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python3
-""" Use mypy to validate the following piece of code
-    and apply any necessary changes.
-    def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-        zoomed_in: Tuple = [
-            item for item in lst
-            for i in range(factor)
-        ]
-        return zoomed_in
+"""Use mypy to validate the following piece of code
+and apply any necessary changes.
+"""
 
-
-    array = [12, 72, 91]
-
-    zoom_2x = zoom_array(array)
-
-    zoom_3x = zoom_array(array, 3.0) """
-from typing import Tuple, List
+from typing import Tuple, List, Any
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """ Type Checking """
+    """Corrected annotations"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
@@ -30,4 +19,4 @@ array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3
+zoom_3x = zoom_array(array, 3)
