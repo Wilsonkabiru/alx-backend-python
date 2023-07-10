fe67d5cfe9fb47fe77492f27ee397537b9c146b7
Carlos Wilson Niqx <wkabiru19@gmail.com>
2023-07-10 15:58:05 +0300

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""Contains a method that returns a task"""
+'''
+Task Import wait_random from 0-basic_async_syntax.
+'''
+
 import asyncio
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """
-    Returns a task that waits for a random number of seconds
-    Args:
-        max_delay: maximum number of seconds that the task will wait
-    Returns: an asyncio.Task object
-    """
+    '''
+    To create an asynchronous task for wait_random.
+    '''
     return asyncio.create_task(wait_random(max_delay))
